050d8c7604f3d243057bc0d34f31e0738d4cfe89
unknown <beratcan_Acil@hotmail.se>
2019-02-12 22:12:09 +0100

    routing & create-advertisement component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { CreateAdvertisementComponent } from './components/create-advertisement/create-advertisement.component';
+
+const appRoutes: Routes = [
+  { path: 'create-advertisement', component: CreateAdvertisementComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    CreateAdvertisementComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes, { useHash: true }),
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/create-advertisement/create-advertisement.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+//import { AngularFirestore } from '@angular/fire/firestore';
+
+@Component({
+  selector: 'app-create-advertisement',
+  templateUrl: './create-advertisement.component.html',
+  styleUrls: ['./create-advertisement.component.css']
+})
+export class CreateAdvertisementComponent implements OnInit {
+
+  todo = {
+    text: "Learn Firebase",
+    completed: false
+  }
+
+  constructor(
+    //private firestore: AngularFirestore
+  ) { }
+
+  ngOnInit() {
+    //this.firestore.collection('todo').add(this.todo);
+  }
+
+}
