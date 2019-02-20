2c75303cdb18be86e02c49dae0fb2bcc3137b19b
unknown <beratcan_Acil@hotmail.se>
2019-02-20 20:19:45 +0100

    Improvement of forms

--- src/app/components/create-advertisement/create-advertisement.component.ts
+++ src/app/components/create-advertisement/create-advertisement.component.ts
@@ -13,6 +13,12 @@ export class CreateAdvertisementComponent implements OnInit {
   myForm: FormGroup;
   dateDATA = new Date();
 
+  labels = ['Volvo', 'Volkswagen', 'Mercedes-Benz', 'BMW', 'Audi', 'Ford', 'Renault', 'Chevrolet', 'SEAT', 'Hyundai', 'Honda', 'Citroen', 'Annat'];
+  fuels = ['Bensin', 'Diesel', 'El', 'Miljöbränsle/Hybrid'];
+  bodys = ['Småbil', 'Sedan', 'Halvkombi', 'Kombi', 'Coupe', 'Cab', 'SUV', 'Familjebuss', 'Yrkesfordon'];
+  gearboxes = ['Automat', 'Manuell'];
+  places = ['Blekinge', 'Dalarnas', 'Gotlands', 'Gävleborgs', 'Hallands', 'Jämtlands', 'Jönköpings', 'Kalmar', 'Kronobergs', 'Norrbottens', 'Skåne', 'Stockholms', 'Södermanlands', 'Uppsala', 'Värmlands', 'Västerbottens', 'Västernorrlands', 'Västmanlands', 'Västra Götalands', 'Örebro', 'Östergötlands'];
+
   constructor(
     private firestore: AngularFirestore,
     private fb: FormBuilder,
